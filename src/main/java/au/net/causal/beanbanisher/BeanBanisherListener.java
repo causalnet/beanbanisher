070a553bb0ef8a0e0fbb7d6ce607621d6d24c00b
Peter Runge <prunge@causal.net.au>
2023-07-25 22:59:54 +1000

    Configurable bean banishing, by name or by class.

--- src/main/java/au/net/causal/beanbanisher/BeanBanisherListener.java
+++ src/main/java/au/net/causal/beanbanisher/BeanBanisherListener.java
@@ -4,12 +4,21 @@ import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class BeanBanisherListener implements SpringApplicationRunListener
 {
     @Override
     public void contextPrepared(ConfigurableApplicationContext context)
     {
-        ((BeanDefinitionRegistry)context).registerBeanDefinition("beanbanisher", BeanDefinitionBuilder.genericBeanDefinition(BeanBanisherRegistryPostProcessor.class).getBeanDefinition());
+        ConfigurableEnvironment env = context.getEnvironment();
+        List<String> beanNames = Arrays.asList(env.getProperty("beanbanisher.beanNames", String[].class, new String[0]));
+        List<String> beanClassNames = Arrays.asList(env.getProperty("beanbanisher.beanClassNames", String[].class, new String[0]));
+
+        ((BeanDefinitionRegistry)context).registerBeanDefinition("beanbanisher", BeanDefinitionBuilder.genericBeanDefinition(BeanBanisherRegistryPostProcessor.class,
+             () -> new BeanBanisherRegistryPostProcessor(beanNames, beanClassNames)).getBeanDefinition());
     }
 }
--- src/main/java/au/net/causal/beanbanisher/BeanBanisherRegistryPostProcessor.java
+++ src/main/java/au/net/causal/beanbanisher/BeanBanisherRegistryPostProcessor.java
@@ -7,11 +7,23 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
 public class BeanBanisherRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor
 {
     private static final Logger log = Logger.getLogger(BeanBanisherRegistryPostProcessor.class.getName());
+
+    private final Set<String> banishedBeanNames;
+    private final Set<String> banishedBeanClassNames;
+
+    public BeanBanisherRegistryPostProcessor(Collection<String> banishedBeanNames, Collection<String> banishedBeanClassNames)
+    {
+        this.banishedBeanNames = new LinkedHashSet<>(banishedBeanNames);
+        this.banishedBeanClassNames = new LinkedHashSet<>(banishedBeanClassNames);
+    }
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
@@ -24,16 +36,44 @@ public class BeanBanisherRegistryPostProcessor implements BeanDefinitionRegistry
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry)
     throws BeansException
     {
-        try
+        //Banish beans by name
+        for (String beanName : banishedBeanNames)
         {
-            BeanDefinition beanDefinition = registry.getBeanDefinition("myBean");
-            log.info("Banishing bean: " + beanDefinition);
-            registry.removeBeanDefinition("myBean");
-        }
-        catch (NoSuchBeanDefinitionException e)
-        {
-            //No such bean
+            try
+            {
+                BeanDefinition beanDefinition = registry.getBeanDefinition(beanName);
+                banishBean(beanName, beanDefinition, registry);
+            }
+            catch (NoSuchBeanDefinitionException e)
+            {
+                //No such bean
+            }
         }
 
+        //Banish beans by type
+        if (!banishedBeanClassNames.isEmpty())
+        {
+            for (String beanName : registry.getBeanDefinitionNames())
+            {
+                try
+                {
+                    BeanDefinition beanDefinition = registry.getBeanDefinition(beanName);
+                    Class<?> beanClass = beanDefinition.getResolvableType().getRawClass();
+                    if (beanClass != null && banishedBeanClassNames.contains(beanClass.getName()))
+                        banishBean(beanName, beanDefinition, registry);
+                }
+                catch (NoSuchBeanDefinitionException e)
+                {
+                    //Shouldn't happen since we are using a bean name from the registry, but better safe than sorry
+                }
+
+            }
+        }
+    }
+
+    private void banishBean(String beanName, BeanDefinition beanDefinition, BeanDefinitionRegistry registry)
+    {
+        log.info("Banishing bean: " + beanDefinition);
+        registry.removeBeanDefinition(beanName);
     }
 }
